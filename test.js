b7f208e1c0bc15acc909c4b0269ef2831ff14afc
Charlike Mike Reagent <mameto_100@mail.bg>
2014-06-19 06:04:51 +0300

    0.0.1

---
+++ test.js
@@ -0,0 +1,43 @@
+/**
+ * gulp-octet - test.js
+ * Copyright (c) 2014
+ * MIT Licensed
+ *
+ * @author  Charlike Mike Reagent (@tunnckoCore)
+ * @api private
+ */
+'use strict';
+var assert = require('assert');
+var gutil = require('gulp-util');
+var gulpOctet = require('./index');
+var user = { name: 'Charlike' };
+
+
+describe('octet: should compile octet templates', function(){
+  it('should support locals', function (done) {
+    var stream = gulpOctet({user: user});
+    stream.on('data', function (data) {
+      assert.equal(data.contents.toString(), '<p>Charlike</p>');
+      done();
+    });
+
+    stream.write(new gutil.File({
+      contents: new Buffer('<p><%this.user.name%></p>')
+    }));
+
+    stream.end();
+  });
+  it('should support helpers', function (done) {
+    var stream = gulpOctet({user: user, uppercase: function (str) {return str.toUpperCase()}});
+    stream.on('data', function (data) {
+      assert.equal(data.contents.toString(), '<p>CHARLIKE</p>');
+      done();
+    });
+
+    stream.write(new gutil.File({
+      contents: new Buffer('<p><%this.uppercase(this.user.name)%></p>')
+    }));
+
+    stream.end();
+  });
+});
